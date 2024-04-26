b9a2201672755b10ba61af30f40a4499ab27d740
Edu Moreno <e.m.morenolp@gmail.com>
2024-04-25 21:08:46 -0300

    modificaciones varias

--- script.js
+++ script.js
@@ -1,7 +1,20 @@
-console.log("hola")
+console.log("Hola Mundo")
 
 document.addEventListener("DOMContentLoaded", function() {
-    
-    let habilidades = document.getElementById("habilidades");
+  const sections = document.querySelectorAll('section');
 
+  function toggleSectionVisibility() {
+      sections.forEach(section => {
+          const rect = section.getBoundingClientRect();
+          if (rect.top >= 0 && rect.bottom <= window.innerHeight) {
+              section.classList.add('visible');
+          } else {
+              section.classList.remove('visible');
+          }
+      });
+  }
+
+  // Ejecutar la función cuando se carga la página y se desplaza
+  window.addEventListener('load', toggleSectionVisibility);
+  window.addEventListener('scroll', toggleSectionVisibility);
 });
