0004db3e135b369b830eafc3558214d93f4eacc0
Edu Moreno <e.m.morenolp@gmail.com>
2024-04-26 21:07:23 -0300

    add icons, modificaciones raras

--- script.js
+++ script.js
@@ -4,17 +4,9 @@ document.addEventListener("DOMContentLoaded", function() {
   const sections = document.querySelectorAll('section');
 
   function toggleSectionVisibility() {
-      sections.forEach(section => {
-          const rect = section.getBoundingClientRect();
-          if (rect.top >= 0 && rect.bottom <= window.innerHeight) {
-              section.classList.add('visible');
-          } else {
-              section.classList.remove('visible');
-          }
-      });
+      
   }
 
   // Ejecutar la función cuando se carga la página y se desplaza
-  window.addEventListener('load', toggleSectionVisibility);
-  window.addEventListener('scroll', toggleSectionVisibility);
+  
 });
